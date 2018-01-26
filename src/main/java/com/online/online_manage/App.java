6618a9488fe860abdd4d19f634ac2052113bb247
shuai.li <shuai.li@mei1.com>
2018-01-26 09:13:41 +0800

    init

---
+++ src/main/java/com/online/online_manage/App.java
@@ -0,0 +1,13 @@
+package com.online.online_manage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
